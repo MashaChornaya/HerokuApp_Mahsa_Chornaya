dd4048e67a653670223fc1bb8fdac3501d03688c
MashaChornaya <105201355+MashaChornaya@users.noreply.github.com>
2022-07-19 21:52:53 +0300

    dz10-third commit [1) в fileUploadTest добавила явное ожидание для загрузки файла 2)в dynamicControlsTest  добавила явное ожидание для появления надписи it's gone в чекбоксе и для появления надписи it's enabled и добавила проверку на то,что чек-бокса нет

--- src/test/java/FileUploadTest.java
+++ src/test/java/FileUploadTest.java
@@ -1,30 +1,35 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-public class FileUploadTest extends BaseTest{
+public class FileUploadTest extends BaseTest {
     @BeforeMethod
     public void navigate() {
 
         driver.get("http://the-internet.herokuapp.com/upload");
     }
+
     @Test
     public void fileUploadTest() {
 
-        String filePath="C:/Program Files/Java/Sharelane/HerokuApp_Mahsa_Chornaya/src/resourses/Screenshot_1.png";
-        WebElement chooseInput=driver.findElement(By.cssSelector("#file-upload"));
-        Assert.assertTrue(chooseInput.isDisplayed());
-        chooseInput.sendKeys(filePath);
+        WebElement fileUploadInput = driver.findElement(By.cssSelector("#file-upload"));
+        Assert.assertTrue(fileUploadInput.isDisplayed());
+        fileUploadInput.sendKeys(System.getProperty("user.dir") + "/src/resourses/Screenshot_1.png");
 
-        isElementPresent(By.xpath("//span[@data-dz-name and text()]"));
+        WebElement uploadFile = (new WebDriverWait(driver, 5))
+                .until(ExpectedConditions.presenceOfElementLocated(By.cssSelector("#file-upload")));
+        Assert.assertTrue(uploadFile.isDisplayed());
 
-        WebElement uploadButton=driver.findElement(By.cssSelector(".button"));
+        WebElement uploadButton = driver.findElement(By.cssSelector(".button"));
         Assert.assertTrue(uploadButton.isEnabled());
         uploadButton.click();
 
-       WebElement uploadedText=driver.findElement(By.cssSelector("#uploaded-files"));
-       Assert.assertEquals(uploadedText.getText(),"Screenshot_1.png");
+        WebElement uploadedText = driver.findElement(By.cssSelector("#uploaded-files"));
+        Assert.assertEquals(uploadedText.getText(), "Screenshot_1.png");
 
-}}
+    }
+}
